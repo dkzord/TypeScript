7fb7165fde029d28d53ed4d9b3167b409674f845
Fernando Cavalcanti <comododk@gmail.com>
2022-10-25 23:30:26 -0300

    feat: init class OO

---
+++ Module 01 - Init/src/A15-Class/index.ts
@@ -0,0 +1,36 @@
+export class Empresa {
+  readonly nome: string; // PUBLIC IS DEFAULT
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const qtdColaboradores of this.colaboradores) {
+      console.log(qtdColaboradores);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('DK Corp', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Fernando', 'Cavalcanti');
+const colaborador3 = new Colaborador('João', 'Victor');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(new Colaborador('Gabriel', 'Paulo'));
+empresa1.adicionaColaborador(colaborador3);
+
+empresa1.mostrarColaboradores();
